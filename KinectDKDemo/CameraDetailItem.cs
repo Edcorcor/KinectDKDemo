703cd70f434580f64f4136298905ab6d34861ea4
EJCorcoran <EJCorcoran@users.noreply.github.com>
2021-01-09 16:37:01 -0500

    Add project files.

---
+++ KinectDKDemo/CameraDetailItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinectDKDemo
+{
+    public class CameraDetailItem : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+
+    }
+}
---
+++ KinectDKDemo/KinectViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinectDKDemo
+{
+    public class KinectViewModel : INotifyPropertyChanged
+    {
+        #region Event
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion Event
+    }
+}
